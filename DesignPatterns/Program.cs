0626455256f8e6961f0598579dc2e371142c5297
Jacek Lipiec <jacek.b.lipiec@gmail.com>
2018-11-05 00:46:03 +0100

    Expand TODO list

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -12,33 +12,60 @@ using DesignPatterns.SingletonPattern;
 using DesignPatterns.StrategyPattern;
 using DesignPatterns.TemplateMethodPattern;
 
+#pragma warning disable SA1515
+#pragma warning disable SA1512
 namespace DesignPatterns {
   public static class Program {
     private static readonly ConsoleUtilities ConsoleUtilities = new ConsoleUtilities();
 
+    // TODO: Provide readme file with observations & usage strategies and notes how I understand the patterns
+    // TODO: Confront understanding with "Design Patterns: Elements of Reusable Object-Oriented Software" book
+    // TODO: Consider UML diagrams
     public static void Main() {
-      // Order kept as in Christopher Okhravi series
-      // TODO: Provide readme file with observations & usage strategies and notes how I understand the patterns
-      // TODO: Confront understanding with GoF book
-      // TODO: Consider UML diagrams
-      ConsoleUtilities.DisplayInBlock(new StrategyPatternSample());
-      ConsoleUtilities.DisplayInBlock(new ObserverPatternWithSubscriberSample());
-      ConsoleUtilities.DisplayInBlock(new DecoratorPatternSample());
-      ConsoleUtilities.DisplayInBlock(new FactoryMethodPatternSample());
+      CreationalPatterns();
+      StructuralPattterns();
+      BehavioralPatterns();
+      OtherPatterns();
+      // TODO: Consider Concurrency patterns
+      // TODO: Consider Architectural patterns
+
+      ConsoleUtilities.PauseApp();
+    }
+
+    private static void CreationalPatterns() {
       ConsoleUtilities.DisplayInBlock(new AbstractFactoryPatternSample());
+      ConsoleUtilities.DisplayInBlock(new FactoryMethodPatternSample());
       ConsoleUtilities.DisplayInBlock(new SingletonPatternSample());
-      ConsoleUtilities.DisplayInBlock(new CommandPatternSample());
+      // TODO: Builder pattern
+      // TODO: Prototype pattern
+    }
+
+    private static void StructuralPattterns() {
       ConsoleUtilities.DisplayInBlock(new AdapterPatternSample());
+      ConsoleUtilities.DisplayInBlock(new BridgePatternSample());
+      ConsoleUtilities.DisplayInBlock(new CompositePatternSample());
+      ConsoleUtilities.DisplayInBlock(new DecoratorPatternSample());
       ConsoleUtilities.DisplayInBlock(new FacadePatternSample());
       ConsoleUtilities.DisplayInBlock(new ProxyPatternSample());
-      ConsoleUtilities.DisplayInBlock(new BridgePatternSample());
-      ConsoleUtilities.DisplayInBlock(new TemplateMethodPatternSample());
-      ConsoleUtilities.DisplayInBlock(new CompositePatternSample());
+      // TODO: Flyweight pattern
+    }
 
+    private static void BehavioralPatterns() {
+      ConsoleUtilities.DisplayInBlock(new StrategyPatternSample());
+      ConsoleUtilities.DisplayInBlock(new ObserverPatternWithSubscriberSample());
+      ConsoleUtilities.DisplayInBlock(new CommandPatternSample());
+      ConsoleUtilities.DisplayInBlock(new TemplateMethodPatternSample());
+      // TODO: Chain of responsibility pattern
+      // TODO: Interpreter pattern
       // TODO: Iterator pattern
+      // TODO: Mediator pattern
+      // TODO: Memento pattern
       // TODO: State pattern
+      // TODO: Visitor pattern
+    }
+
+    private static void OtherPatterns() {
       // TODO: Null Object pattern
-      ConsoleUtilities.PauseApp();
     }
   }
 }
