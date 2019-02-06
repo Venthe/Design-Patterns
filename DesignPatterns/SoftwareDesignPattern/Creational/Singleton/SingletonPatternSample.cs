362761c32fea1b3fba27ff26ce0bdea5bf4d12a4
Jon Cederqvist <jon.cederqvist@gmail.com>
2019-02-06 20:47:35 +0100

    Added README.md to Singleton pattern and made singleton c.tor private.

--- DesignPatterns/SoftwareDesignPattern/Creational/Singleton/SingletonPatternSample.cs
+++ DesignPatterns/SoftwareDesignPattern/Creational/Singleton/SingletonPatternSample.cs
@@ -3,8 +3,8 @@
 namespace DesignPatterns.SoftwareDesignPattern.Creational.Singleton {
   public class SingletonPatternSample : IDesignPatternSample {
     public void ShowSample() {
-      var singleton1 = new SampleSingleton();
-      var singleton2 = new SampleSingleton();
+      var singleton1 = SampleSingleton.GetInstance();
+      var singleton2 = SampleSingleton.GetInstance();
 
       Console.WriteLine($"This should be true: {singleton1.GetInstance().Equals(singleton2.GetInstance())}");
     }
